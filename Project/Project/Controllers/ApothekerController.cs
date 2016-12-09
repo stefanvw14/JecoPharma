4904f1b2685c2e09c967f91fc8248ff8fe26f0d1
Stefan van Weerden <stefanweerden@hotmail.com>
2016-12-09 14:22:40 +0100

    Apotheker Controller gemaakt, structuur Apotheker terug gepakt vanaf Bit Bucket

--- Project/Project/Controllers/ApothekerController.cs
+++ Project/Project/Controllers/ApothekerController.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Project.Controllers
 {
-    public class ApothekerController
+    public class ApothekerController : Controller
     {
+       public ActionResult Index()
+        {
+            string datum = string.Format("{0:D}", DateTime.Now);
+            ViewBag.Datum = datum;
+            return View();
+        }
+
+        public ActionResult EAVSamenstellen()
+        {
+            return View();
+        }
+
+        public ActionResult EAVWijzigen()
+        {
+            return View();
+        }
+
+        /*public ActionResult ConflictVerwijderen()
+        {
+            return View();
+        }*/
     }
 }
